28c09aa20c56bc86464020f22f6d0899438fe5d8
Kelvin Aguilar <k.aguilar04@proton.me>
2025-09-10 11:34:14 -0400

    safe commit

--- my_unit_converter/members/models.py
+++ my_unit_converter/members/models.py
@@ -8,3 +8,7 @@ class Member(models.Model):
 
 
 # Create your models here.
+class UnitInformation(models.Model):
+	fromUnit = models.CharField(max_length=255)
+	toUnit = models.CharField(max_length=255)
+	unitAmount = models.DecimalField(decimal_places=3, max_digits=7)
